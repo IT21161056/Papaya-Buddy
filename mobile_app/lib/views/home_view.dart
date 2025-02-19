2ffdffa8e9283fe33faca7f57b7d646d634c9489
anojnuclei <anoj@nuclei.tech>
2025-02-19 15:52:03 +0530

    feat: create splash screen, home screen and the bottom navigation bar

---
+++ mobile_app/lib/views/home_view.dart
@@ -0,0 +1,44 @@
+import 'package:flutter/material.dart';
+
+class HomeView extends StatefulWidget {
+  const HomeView({super.key});
+
+  @override
+  _HomeViewState createState() => _HomeViewState();
+}
+
+class _HomeViewState extends State<HomeView> {
+  int _selectedIndex = 0;
+
+  // List of pages for navigation
+  static const List<Widget> _pages = <Widget>[
+    Center(child: Text('Home Page', style: TextStyle(fontSize: 20))),
+    Center(child: Text('Disease View', style: TextStyle(fontSize: 20))),
+    Center(child: Text('Profile Page', style: TextStyle(fontSize: 20))),
+  ];
+
+  void _onItemTapped(int index) {
+    setState(() {
+      _selectedIndex = index;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: const Text('Flutter App')),
+      body: Center(child: _pages[_selectedIndex]),
+      bottomNavigationBar: BottomNavigationBar(
+        items: const <BottomNavigationBarItem>[
+          BottomNavigationBarItem(icon: Icon(Icons.home), label: 'Home'),
+          BottomNavigationBarItem(
+              icon: Icon(Icons.local_hospital), label: 'Diseases'),
+          BottomNavigationBarItem(icon: Icon(Icons.person), label: 'Profile'),
+        ],
+        currentIndex: _selectedIndex,
+        selectedItemColor: Colors.blue,
+        onTap: _onItemTapped,
+      ),
+    );
+  }
+}
