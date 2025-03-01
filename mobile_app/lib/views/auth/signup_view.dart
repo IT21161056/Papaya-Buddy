f9e78bd1da0c271f173f8c73df72b51e674d09a9
anojnuclei <anoj@nuclei.tech>
2025-03-01 14:47:08 +0530

    feat: signup page is recreated

--- mobile_app/lib/views/auth/signup_view.dart
+++ mobile_app/lib/views/auth/signup_view.dart
@@ -1,8 +1,8 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_svg/flutter_svg.dart';
 
 class SignupView extends StatefulWidget {
   const SignupView({super.key});
-
   @override
   _SignupViewState createState() => _SignupViewState();
 }
@@ -17,7 +17,6 @@ class _SignupViewState extends State<SignupView> {
 
   bool _isPasswordVisible = false;
   bool _isConfirmPasswordVisible = false;
-
   void _signUp() {
     if (_formKey.currentState!.validate()) {
       // Proceed with signup logic
@@ -31,157 +30,268 @@ class _SignupViewState extends State<SignupView> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Center(
-        child: SingleChildScrollView(
-          padding: const EdgeInsets.symmetric(horizontal: 24),
-          child: Form(
-            key: _formKey,
-            child: Column(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: [
-                // App Logo
-                const Icon(Icons.person_add, size: 80, color: Colors.green),
-                const SizedBox(height: 16),
-
-                const Text(
-                  "Create Account",
-                  style: TextStyle(fontSize: 24, fontWeight: FontWeight.bold),
-                ),
-                const SizedBox(height: 32),
-
-                // Full Name Field
-                TextFormField(
-                  controller: _nameController,
-                  decoration: InputDecoration(
-                    labelText: "Full Name",
-                    prefixIcon: Icon(Icons.person),
-                    border: OutlineInputBorder(
-                        borderRadius: BorderRadius.circular(10)),
-                  ),
-                  keyboardType: TextInputType.name,
-                ),
-                const SizedBox(height: 16),
-
-                // Email Field
-                TextFormField(
-                  controller: _emailController,
-                  decoration: InputDecoration(
-                    labelText: "Email",
-                    prefixIcon: Icon(Icons.email),
-                    border: OutlineInputBorder(
-                      borderRadius: BorderRadius.circular(10),
+      appBar: AppBar(
+        centerTitle: true,
+        backgroundColor: Colors.white,
+        elevation: 0,
+        leading: IconButton(
+          icon: Icon(Icons.arrow_back_ios, color: Colors.black),
+          iconSize: 16,
+          alignment: Alignment.center,
+          onPressed: () {
+            Navigator.pop(context);
+          },
+        ),
+        title: Text(
+          "Create Account",
+          style: TextStyle(
+            fontWeight: FontWeight.bold,
+            color: Colors.black,
+            fontSize: 20,
+          ),
+        ),
+      ),
+      backgroundColor: Colors.white,
+      body: Padding(
+        padding: const EdgeInsets.all(20),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            const Center(
+              child: Column(
+                children: [
+                  Text(
+                    "Join PapayaBuddy to diagnose and treat plant diseases",
+                    textAlign: TextAlign.center,
+                    style: TextStyle(
+                      fontSize: 14,
+                      color: Colors.grey,
                     ),
                   ),
-                  keyboardType: TextInputType.emailAddress,
-                  validator: (value) {
-                    if (value == null || value.isEmpty) {
-                      return "Please enter your email";
-                    }
-                    if (!RegExp(
-                            r"^[a-zA-Z0-9.a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9]+\.[a-zA-Z]+")
-                        .hasMatch(value)) {
-                      return "Enter a valid email";
-                    }
-                    return null;
-                  },
-                ),
-                const SizedBox(height: 16),
-
-                // Password Field
-                TextFormField(
-                  controller: _passwordController,
-                  obscureText: !_isPasswordVisible,
-                  decoration: InputDecoration(
-                    labelText: "Password",
-                    prefixIcon: const Icon(Icons.lock),
-                    suffixIcon: IconButton(
-                      icon: Icon(_isPasswordVisible
-                          ? Icons.visibility
-                          : Icons.visibility_off),
-                      onPressed: () {
-                        setState(() {
-                          _isPasswordVisible = !_isPasswordVisible;
-                        });
-                      },
+                ],
+              ),
+            ),
+            const SizedBox(height: 30),
+            TextField(
+              controller: _nameController,
+              decoration: InputDecoration(
+                prefixIcon: Padding(
+                  padding: const EdgeInsets.all(
+                      12), // Adjust padding for better alignment
+                  child: SizedBox(
+                    height: 18, // Adjust icon size
+                    width: 18,
+                    child: SvgPicture.asset(
+                      'assets/icons/user.svg', // Replace with correct icon
+                      height: 12,
+                      width: 12,
+                      color: Color(0xFF64748B),
                     ),
-                    border: OutlineInputBorder(
-                        borderRadius: BorderRadius.circular(10)),
                   ),
-                  validator: (value) {
-                    if (value == null || value.isEmpty) {
-                      return "Please enter your password";
-                    }
-                    if (value.length < 6) {
-                      return "Password must be at least 6 characters long";
-                    }
-                    return null;
-                  },
                 ),
-                const SizedBox(height: 16),
-
-                // Confirm Password Field
-                TextFormField(
-                  controller: _confirmPasswordController,
-                  obscureText: !_isConfirmPasswordVisible,
-                  decoration: InputDecoration(
-                    labelText: "Confirm Password",
-                    prefixIcon: const Icon(Icons.lock),
-                    suffixIcon: IconButton(
-                      icon: Icon(_isConfirmPasswordVisible
-                          ? Icons.visibility
-                          : Icons.visibility_off),
-                      onPressed: () {
-                        setState(() {
-                          _isConfirmPasswordVisible =
-                              !_isConfirmPasswordVisible;
-                        });
-                      },
+                hintText: "Full Name",
+                filled: true,
+                fillColor: Color(0xFFF8FAFC),
+                enabledBorder: OutlineInputBorder(
+                  borderRadius: BorderRadius.circular(12),
+                  borderSide: const BorderSide(
+                      color: Color(0xFFE2E8F0)), // Border color added
+                ),
+                focusedBorder: OutlineInputBorder(
+                  borderRadius: BorderRadius.circular(12),
+                  borderSide: const BorderSide(
+                      color: Color(0xFFE2E8F0),
+                      width: 2), // Slightly thicker on focus
+                ),
+              ),
+            ),
+            const SizedBox(height: 15),
+            TextField(
+              controller: _emailController,
+              decoration: InputDecoration(
+                prefixIcon: Padding(
+                  padding: const EdgeInsets.all(
+                      12), // Adjust padding for better alignment
+                  child: SizedBox(
+                    height: 18, // Adjust icon size
+                    width: 18,
+                    child: SvgPicture.asset(
+                      'assets/icons/lucide_mail.svg', // Replace with correct icon
+                      height: 12,
+                      width: 12,
+                      color: Color(0xFF64748B),
                     ),
-                    border: OutlineInputBorder(
-                        borderRadius: BorderRadius.circular(10)),
                   ),
-                  validator: (value) {
-                    if (value == null || value.isEmpty) {
-                      return "Please confirm your password";
-                    }
-                    if (value != _passwordController.text) {
-                      return "Passwords do not match";
-                    }
-                    return null;
+                ),
+                hintText: "Email",
+                filled: true,
+                fillColor: Color(0xFFF8FAFC),
+                enabledBorder: OutlineInputBorder(
+                  borderRadius: BorderRadius.circular(12),
+                  borderSide: const BorderSide(
+                      color: Color(0xFFE2E8F0)), // Border color added
+                ),
+                focusedBorder: OutlineInputBorder(
+                  borderRadius: BorderRadius.circular(12),
+                  borderSide: const BorderSide(
+                      color: Color(0xFFE2E8F0),
+                      width: 2), // Slightly thicker on focus
+                ),
+              ),
+            ),
+
+            const SizedBox(height: 15),
+
+            // Password
+            TextField(
+              controller: _passwordController,
+              obscureText: !_isPasswordVisible,
+              decoration: InputDecoration(
+                prefixIcon: Padding(
+                  padding: const EdgeInsets.all(
+                      12), // Adjust padding for better alignment
+                  child: SizedBox(
+                    height: 18, // Adjust icon size
+                    width: 18,
+                    child: SvgPicture.asset(
+                      'assets/icons/lucide_lock.svg', // Replace with correct icon
+                      height: 12,
+                      width: 12,
+                      color: Color(0xFF64748B),
+                    ),
+                  ),
+                ), // Using exact RGBO equivalent
+                suffixIcon: IconButton(
+                  icon: Icon(
+                    _isPasswordVisible
+                        ? Icons.visibility
+                        : Icons.visibility_off,
+                    color: Color(0xFF64748B),
+                  ),
+                  onPressed: () {
+                    setState(() {
+                      _isPasswordVisible = !_isPasswordVisible;
+                    });
                   },
                 ),
-                const SizedBox(height: 24),
+                hintText: "Password",
+                filled: true,
+                fillColor: Color(0xFFF8FAFC),
+                enabledBorder: OutlineInputBorder(
+                  borderRadius: BorderRadius.circular(10),
+                  borderSide: const BorderSide(
+                      color: Color(0xFFE2E8F0)), // Light border
+                ),
+                focusedBorder: OutlineInputBorder(
+                  borderRadius: BorderRadius.circular(10),
+                  borderSide: const BorderSide(
+                      color: Color(0xFFE2E8F0),
+                      width: 2), // Thicker when focused
+                ),
+              ),
+            ),
 
-                // Signup Button
-                SizedBox(
-                  width: double.infinity,
-                  height: 50,
-                  child: ElevatedButton(
-                    onPressed: _signUp,
-                    style: ElevatedButton.styleFrom(
-                      backgroundColor: Colors.blue,
-                      shape: RoundedRectangleBorder(
-                        borderRadius: BorderRadius.circular(10),
+            const SizedBox(height: 15),
+
+            // Confirm Password
+            TextField(
+              controller: _confirmPasswordController,
+              obscureText: !_isConfirmPasswordVisible,
+              decoration: InputDecoration(
+                prefixIcon: Padding(
+                  padding: const EdgeInsets.all(
+                      12), // Adjust padding for better alignment
+                  child: SizedBox(
+                    height: 18, // Adjust icon size
+                    width: 18,
+                    child: SvgPicture.asset(
+                      'assets/icons/lucide_lock.svg', // Replace with correct icon
+                      height: 12,
+                      width: 12,
+                      color: Color(0xFF64748B),
+                    ),
+                  ),
+                ), // Using exact RGBO equivalent
+                suffixIcon: IconButton(
+                  icon: Icon(
+                    _isConfirmPasswordVisible
+                        ? Icons.visibility
+                        : Icons.visibility_off,
+                    color: Color(0xFF64748B),
+                  ),
+                  onPressed: () {
+                    setState(() {
+                      _isConfirmPasswordVisible = !_isConfirmPasswordVisible;
+                    });
+                  },
+                ),
+                hintText: "Confirm Password",
+                filled: true,
+                fillColor: Color(0xFFF8FAFC),
+                enabledBorder: OutlineInputBorder(
+                  borderRadius: BorderRadius.circular(10),
+                  borderSide: const BorderSide(
+                      color: Color(0xFFE2E8F0)), // Light border
+                ),
+                focusedBorder: OutlineInputBorder(
+                  borderRadius: BorderRadius.circular(10),
+                  borderSide: const BorderSide(
+                      color: Color(0xFFE2E8F0),
+                      width: 2), // Thicker when focused
+                ),
+              ),
+            ),
+
+            const SizedBox(height: 30),
+
+            // button
+            ElevatedButton(
+              style: ElevatedButton.styleFrom(
+                backgroundColor: Colors.blue,
+                minimumSize: const Size(double.infinity, 50),
+                shape: RoundedRectangleBorder(
+                  borderRadius: BorderRadius.circular(10),
+                ),
+              ),
+              onPressed: _signUp,
+              child: const Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  Text(
+                    "Create Account",
+                    style: TextStyle(fontSize: 16, color: Colors.white),
+                  ),
+                  SizedBox(width: 8), // Space between text and icon
+                  Icon(Icons.arrow_right_alt,
+                      color: Colors.white), // Add forward icon
+                ],
+              ),
+            ),
+
+            const SizedBox(height: 15),
+            Center(
+              child: RichText(
+                text: TextSpan(
+                  text: "Already have an account? ",
+                  style: const TextStyle(color: Colors.black),
+                  children: [
+                    WidgetSpan(
+                      child: GestureDetector(
+                        onTap: () {},
+                        child: const Text(
+                          "Sign In",
+                          style: TextStyle(
+                            color: Colors.blue,
+                            fontWeight: FontWeight.bold,
+                          ),
+                        ),
                       ),
                     ),
-                    child: const Text(
-                      "Sign Up",
-                      style: TextStyle(fontSize: 18, color: Colors.white),
-                    ),
-                  ),
+                  ],
                 ),
-                const SizedBox(height: 16),
-
-                // Login Link
-                TextButton(
-                  onPressed: () {
-                    Navigator.pushNamed(context, '/login');
-                  },
-                  child: const Text("Already have an account? Login"),
-                ),
-              ],
+              ),
             ),
-          ),
+          ],
         ),
       ),
     );
