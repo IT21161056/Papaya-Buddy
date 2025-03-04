380f953c665be10b0efa6f8f653c9bd53abbd111
Vikum Manchanayake <vikumviraj@vizuamatix.com>
2025-03-04 23:32:08 +0530

    navigation to healthy UI when predicts Healthy Leaf & Fruit

--- mobile_app/lib/views/healthyView/healthy_view.dart
+++ mobile_app/lib/views/healthyView/healthy_view.dart
@@ -1,31 +1,38 @@
 import 'package:flutter/material.dart';
 import 'package:carousel_slider/carousel_slider.dart';
-import 'package:mobile_app/widgets/healthy_view.widgets/recommendations.dart';
+import 'package:mobile_app/views/treatment_view.dart'; // Ensure this import is correct
 
 class HealthyView extends StatelessWidget {
-  @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      debugShowCheckedModeBanner: false,
-      home: HealthyScreen(),
-    );
-  }
-}
-
-class HealthyScreen extends StatelessWidget {
+  final String diseaseName; // Add this line to accept the diseaseName prop
   final List<String> imagePaths = [
     'assets/r2.jpg',
     'assets/r2.jpg',
+    'assets/r2.jpg',
   ];
+
+  // Add a constructor to accept diseaseName
+  HealthyView({Key? key, this.diseaseName = 'Healthy Leaf'}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         title: Text("Diagnosis"),
+        leading: IconButton(
+          icon: Icon(Icons.arrow_back_ios, color: Colors.black),
+          iconSize: 16,
+          alignment: Alignment.center,
+          onPressed: () {
+            Navigator.pop(context);
+          },
+        ),
         actions: [
           IconButton(
             icon: Icon(Icons.share),
+            onPressed: () {},
+          ),
+          IconButton(
+            icon: Icon(Icons.more_vert),
             onPressed: () {},
           ),
         ],
@@ -36,56 +43,124 @@ class HealthyScreen extends StatelessWidget {
           crossAxisAlignment: CrossAxisAlignment.start,
           children: [
             Text(
-              "Healthy Plant",
+              diseaseName, // Use the diseaseName parameter here
               style: TextStyle(fontSize: 22, fontWeight: FontWeight.bold),
             ),
+            SizedBox(height: 4),
             Text(
-              "Great job! 🎉",
-              style: TextStyle(color: Colors.green, fontSize: 16),
+              "Healthy", // Indicate that the plant is healthy
+              style: TextStyle(fontSize: 16, color: Colors.green),
             ),
-            SizedBox(height: 10),
-            CarouselSlider(
-              options: CarouselOptions(
-                height: 240,
-                enlargeCenterPage: true,
-                enableInfiniteScroll: true,
-                autoPlay: true,
-                scrollDirection: Axis.horizontal,
+            SizedBox(height: 12),
+            Stack(
+              children: [
+                CarouselSlider(
+                  options: CarouselOptions(
+                      height: 180,
+                      enableInfiniteScroll: true,
+                      enlargeCenterPage: true,
+                      autoPlay: true),
+                  items: imagePaths.map((path) {
+                    return ClipRRect(
+                      borderRadius: BorderRadius.circular(10),
+                      child: Image.asset(
+                        path,
+                        width: double.infinity,
+                        fit: BoxFit.cover,
+                      ),
+                    );
+                  }).toList(),
+                ),
+                Positioned(
+                  bottom: 8,
+                  right: 8,
+                  child: Icon(
+                    Icons.swipe, // Hand icon for scroll indicator
+                    color: Colors.white,
+                    size: 24,
+                  ),
+                ),
+              ],
+            ),
+            SizedBox(height: 8),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.end,
+              children: [
+                Text("${imagePaths.length} photos",
+                    style: TextStyle(color: Colors.grey)),
+              ],
+            ),
+            SizedBox(height: 12),
+            Container(
+              padding: EdgeInsets.all(12),
+              decoration: BoxDecoration(
+                color: Colors.blue[50],
+                borderRadius: BorderRadius.circular(10),
               ),
-              items: imagePaths.map((path) {
-                return Container(
-                  margin: EdgeInsets.symmetric(horizontal: 5.0),
-                  decoration: BoxDecoration(
-                    borderRadius: BorderRadius.circular(15.0),
-                    image: DecorationImage(
-                      image: AssetImage(path),
-                      fit: BoxFit.cover,
+              child: Row(
+                children: [
+                  Icon(Icons.info, color: Colors.blue),
+                  SizedBox(width: 8),
+                  Expanded(
+                    child: Text(
+                      "Your plant is healthy! Keep up the good work.",
+                      style: TextStyle(color: Colors.black),
                     ),
                   ),
-                );
-              }).toList(),
+                ],
+              ),
             ),
-            SizedBox(height: 5),
-            Text("${imagePaths.length} photos",
-                style: TextStyle(color: Colors.grey)),
-            SizedBox(height: 20),
-            RecommendationsSection(),
-            SizedBox(height: 20),
-            SizedBox(
-              width: double.infinity,
-              child: ElevatedButton(
-                style: ElevatedButton.styleFrom(
-                  padding: EdgeInsets.symmetric(vertical: 12),
-                  shape: RoundedRectangleBorder(
-                    borderRadius: BorderRadius.circular(10),
-                  ),
-                  backgroundColor: Colors.blue,
+            SizedBox(height: 12),
+            Text(
+              "Tips to keep it healthy",
+              style: TextStyle(fontSize: 18, fontWeight: FontWeight.bold),
+            ),
+            SizedBox(height: 8),
+            ListTile(
+              leading: ClipRRect(
+                borderRadius: BorderRadius.circular(8),
+                child: Image.asset(
+                  'assets/r2.jpg',
+                  width: 50,
+                  height: 50,
+                  fit: BoxFit.cover,
                 ),
-                onPressed: () {},
-                child: Text(
-                  "Save to your diagnoses",
-                  style: TextStyle(color: Colors.white, fontSize: 16),
+              ),
+              title: Text("Proper Watering"),
+              subtitle: Text("Ensure the plant gets adequate water."),
+              trailing: Icon(Icons.arrow_forward_ios),
+              onTap: () {},
+            ),
+            ListTile(
+              leading: ClipRRect(
+                borderRadius: BorderRadius.circular(8),
+                child: Image.asset(
+                  'assets/r2.jpg',
+                  width: 50,
+                  height: 50,
+                  fit: BoxFit.cover,
                 ),
+              ),
+              title: Text("Sunlight"),
+              subtitle: Text("Provide sufficient sunlight."),
+              trailing: Icon(Icons.arrow_forward_ios),
+              onTap: () {},
+            ),
+            SizedBox(height: 16),
+            ElevatedButton(
+              style: ElevatedButton.styleFrom(
+                backgroundColor: Colors.green,
+                minimumSize: Size(double.infinity, 50),
+              ),
+              onPressed: () {
+                Navigator.push(
+                  context,
+                  MaterialPageRoute(builder: (context) => TreatmentScreen()),
+                );
+              },
+              child: Text(
+                "Save to your diagnoses",
+                style: TextStyle(color: Colors.white),
               ),
             ),
           ],
