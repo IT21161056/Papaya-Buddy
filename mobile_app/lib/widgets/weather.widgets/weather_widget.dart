e00817377770d42583c4f18f9131037f9ff348fa
Pasindu Perakum <99235576+IT21160066@users.noreply.github.com>
2025-03-13 20:49:12 +0530

    missing weather data fixed

--- mobile_app/lib/widgets/weather.widgets/weather_widget.dart
+++ mobile_app/lib/widgets/weather.widgets/weather_widget.dart
@@ -230,7 +230,7 @@ class _ExpandableWeatherCardState extends State<ExpandableWeatherCard> {
                       ),
                       Text(
                         _weather != null
-                            ? '${_weather?.temperature.round()}°C'
+                            ? '${_weather?.getFormattedTemperature()}'
                             : 'Loading Temperature...',
                         style: TextStyle(
                           fontSize: 16,
@@ -280,7 +280,8 @@ class _ExpandableWeatherCardState extends State<ExpandableWeatherCard> {
                               children: [
                                 WeatherInfo(
                                   title: "UV Index",
-                                  value: "${_weather?.uvIndex ?? 0}",
+                                  value:
+                                      _weather?.getUvIndexCategory() ?? "Low",
                                   iconPath: 'assets/icons/lucide_sun.svg',
                                   iconColor: Colors.amber,
                                 ),
