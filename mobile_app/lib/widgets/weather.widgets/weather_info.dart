e4cf850cad03e9f86e1b251a3bf21245721ae3c1
Pasindu Perakum <99235576+IT21160066@users.noreply.github.com>
2025-03-17 11:45:09 +0530

    resolve commented changes

--- mobile_app/lib/widgets/weather.widgets/weather_info.dart
+++ mobile_app/lib/widgets/weather.widgets/weather_info.dart
@@ -1,40 +1,40 @@
-// import 'package:flutter/material.dart';
-// import 'package:flutter_svg/flutter_svg.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_svg/flutter_svg.dart';
 
-// class WeatherInfo extends StatelessWidget {
-//   final String title;
-//   final String value;
-//   final String iconPath;
-//   final Color iconColor;
+class WeatherInfo extends StatelessWidget {
+  final String title;
+  final String value;
+  final String iconPath;
+  final Color iconColor;
 
-//   const WeatherInfo({
-//     super.key,
-//     required this.title,
-//     required this.value,
-//     required this.iconPath,
-//     required this.iconColor,
-//   });
+  const WeatherInfo({
+    super.key,
+    required this.title,
+    required this.value,
+    required this.iconPath,
+    required this.iconColor,
+  });
 
-//   @override
-//   Widget build(BuildContext context) {
-//     return Column(
-//       children: [
-//         SvgPicture.asset(
-//           iconPath,
-//           height: 20,
-//           width: 20,
-//           color: iconColor,
-//         ),
-//         const SizedBox(height: 4),
-//         Text(
-//           title,
-//           style: const TextStyle(fontSize: 12, color: Colors.black54),
-//         ),
-//         Text(
-//           value,
-//           style: const TextStyle(fontSize: 14, fontWeight: FontWeight.bold),
-//         ),
-//       ],
-//     );
-//   }
-// }
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      children: [
+        SvgPicture.asset(
+          iconPath,
+          height: 20,
+          width: 20,
+          color: iconColor,
+        ),
+        const SizedBox(height: 4),
+        Text(
+          title,
+          style: const TextStyle(fontSize: 12, color: Colors.black54),
+        ),
+        Text(
+          value,
+          style: const TextStyle(fontSize: 14, fontWeight: FontWeight.bold),
+        ),
+      ],
+    );
+  }
+}
