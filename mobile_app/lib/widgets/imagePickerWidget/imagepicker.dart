b7977013f2c1e1896ceabd6fc397ffd5729f78fc
Vikum Manchanayake <vikumviraj@vizuamatix.com>
2025-05-01 13:40:16 +0530

    change pest detect model end point

--- mobile_app/lib/widgets/imagePickerWidget/imagepicker.dart
+++ mobile_app/lib/widgets/imagePickerWidget/imagepicker.dart
@@ -63,7 +63,7 @@ class _ImagePickerPageState extends State<ImagePickerPage> {
     try {
       var request = http.MultipartRequest(
         'POST',
-        Uri.parse('http://10.0.2.2:8000/predict'),
+        Uri.parse('http://10.0.2.2:5014/predict-pest-disease'),
       );
       request.files
           .add(await http.MultipartFile.fromPath('file', _image!.path));
